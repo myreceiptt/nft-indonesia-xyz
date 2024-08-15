3c835d7779f3561a4387d5014046a75e4761c3b5
myreceiptt <nota@straight-line.org>
2024-08-15 15:29:54 +0700

    beta

---
+++ src/components/Footer/index.js
@@ -0,0 +1,106 @@
+"use client";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { DribbbleIcon, GithubIcon, LinkedinIcon, TwitterIcon } from "../Icons";
+import Link from "next/link";
+import siteMetadata from "@/src/utils/siteMetaData";
+
+const Footer = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  console.log(errors);
+
+  return (
+    <footer className="mt-16 rounded-2xl bg-dark dark:bg-light m-2 sm:m-10 flex flex-col items-center text-light dark:text-dark">
+      <h3 className="mt-16 font-bold text-center capitalize text-2xl sm:text-3xl lg:text-4xl px-4">
+        Never Ending Activities & Updates
+      </h3>
+      <p className="mt-5 px-4 text-center w-full sm:w-3/5 font-medium text-sm sm:text-base">
+        Subscribe to stay up to date with our latest activities. Join to be one of the 281,603,800 Indonesians, who always follow and learn the latest updates.
+      </p>
+
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="mt-6 w-fit sm:min-w-[384px] flex items-stretch bg-light dark:bg-dark p-1 sm:p-2 rounded mx04"
+      >
+        <input
+          type="email"
+          placeholder="Enter your email"
+          {...register("email", { required: true, maxLength: 80 })}
+          className="w-full bg-transparent pl-2 sm:pl-0 text-dark dark:text-light focus:border-dark dark:focus:border-light focus:ring-0 border-0 border-b-2 mr-2 pb-1"
+        />
+        <button className="bg-dark text-light dark:text-dark dark:bg-light hover:bg-gray dark:hover:bg-gray dark:hover:text-light duration-300 cursor-pointer font-medium rounded px-3 sm:px-5 py-1" type='submit'>
+          Subscribe
+        </button>
+      </form>
+      <div className="flex items-center mt-8">
+        <a
+          href={siteMetadata.twitter}
+          className="inline-block w-6 h-6 mr-4 fill-light"
+          aria-label="Reach us via Twitter"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <GithubIcon className="fill-light dark:fill-dark hover:scale-125 transition-all ease duration-200" />
+        </a>
+        <a
+          href={siteMetadata.twitter}
+          className="inline-block w-6 h-6 mr-4 fill-light"
+          aria-label="Reach us via Twitter"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <GithubIcon className="fill-light dark:fill-dark hover:scale-125 transition-all ease duration-200" />
+        </a>
+        <a
+          href={siteMetadata.twitter}
+          className="inline-block w-6 h-6 mr-4 fill-light"
+          aria-label="Reach us via Twitter"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <GithubIcon className="fill-light dark:fill-dark  hover:scale-125 transition-all ease duration-200" />
+        </a>
+        <a
+          href={siteMetadata.twitter}
+          className="inline-block w-6 h-6 mr-4 fill-light"
+          aria-label="Reach us via Twitter"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <GithubIcon className="fill-light dark:fill-dark hover:scale-125 transition-all ease duration-200" />
+        </a>
+      </div>
+
+      <div className="w-full  mt-16 md:mt-24 relative font-medium border-t-2 border-solid dark:border-dark border-light py-6 px-8 flex  flex-col md:flex-row items-center justify-between">
+        <div className="text-center">
+          &copy; Since 2021{" "}
+          <Link href="/" className="font-bold">
+            NFT Indonesia
+          </Link>
+          .
+        </div>
+        <div className="text-center">
+          All rights reserved decentrally.
+        </div>
+        <div className="text-center">
+          Fueled by{" "}
+          <Link
+            href="https://x.com/zortan_"
+            className="capitalize font-bold"
+            target="_blank"
+          >
+            ZORTAN
+          </Link>
+          's &hearts; for you.
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/utils/siteMetaData.js
@@ -0,0 +1,25 @@
+// please update required information
+const siteMetadata = {
+    title: 'NFT Indonesia dot XYZ',
+    author: 'ZORTAN',
+    headerTitle: 'NFT Indonesia Contents',
+    description: 'Contents created by NFT Indonesia based on all this time activities.',
+    language: 'en-us',
+    theme: 'dark', // system, dark or light
+    siteUrl: 'https://blog.nftindonesia.xyz', // your website URL
+    siteLogo: '/logo.png',
+    socialBanner: '/social-banner.png', // add social banner in the public folder
+    email: 'zortan@nftindonesia.xyz', 
+    twitter: 'https://x.com/NFTindonesia_',
+    instagram: 'https://www.instagram.com/nftindonesia_/',
+    // discord: 'https://discord.gg/3AvJSYPJK6',
+    // farcaster: 'https://warpcast.com/nftindonesia',
+    // hug-collective: 'https://thehug.xyz/collectives/NFTindonesia_',
+    // facebook: 'https://facebook.com',
+    // youtube: 'https://youtube.com/codebucks',
+    // linkedin: 'https://www.linkedin.com/in/codebucks/',
+    // dribbble: 'https://www.dribbble.com',
+    locale: 'en-US',
+  }
+  
+  module.exports = siteMetadata
