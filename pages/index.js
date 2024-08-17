d17d9bb675caa97812cba4cd56730399caa11705
Prof. NOTA <141787406+myreceiptt@users.noreply.github.com>
2024-08-17 13:48:14 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,50 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { Analytics } from "@vercel/analytics/react";
+
 export default function Home() {
   return (
-    <div className="{styles.container}">
-      <head>
+    <div className={styles.container}>
+      <Head>
         <title>NFT Indonesia: Initialization Page!</title>
         <link href="/logo32.png" rel="shortcut icon" />
         <link rel="icon" href="/nft-indonesia-logo.webp" />
         <link rel="apple-touch-icon" href="/nft-indonesia-logo.webp" />
-        //
-        <link rel="shortcut icon" href="/nft-indonesia-logo.webp" />
+        // <link rel="shortcut icon" href="/nft-indonesia-logo.webp" />
         <link rel="canonical" href="https://www.nftindonesia.xyz/" />
+
         <meta
           name="description"
           content="The init page initializes and controls processes. Its primary role is to start processes based on records read from the NFT Indonesia files. NFT Indonesia files usually requests that the init command run the getty command for each line on which process can log in."
         />
-      </head>
+      </Head>
 
       <main>
-        <Analytics />
-        <h1 className="{styles.title}">
+        <Analytics/>
+        <h1 className={styles.title}>
           <a href="https://www.nftindonesia.xyz" target="_blank">
-            {" "}
-            NFT Indonesia{" "}
+            NFT Indonesia
           </a>{" "}
           <code>init</code> Page!
         </h1>
 
-        <p className="{styles.description}">
+        <p className={styles.description}>
           Get started by editing <code>pages/index.js</code>
         </p>
 
-        <iframe
-          src="https://www.youtube.com/watch?v=EbneWlPGRtU"
-          frameborder="0"
-          allowfullscreen
-        />
+        <div className={styles.grid}>
+          <a
+            href="#"
+            className={styles.card}
+            target="_blank"
+          >
+            <h3>This Page &rarr;</h3>
+            <p>An init page contain a link list related to NFT Indonesia.</p>
+          </a>
 
-        <div className="{styles.grid}">
           <a
             href="https://x.com/NFTindonesia_"
-            className="{styles.card}"
+            className={styles.card}
             target="_blank"
           >
             <h3>X - Twitter &rarr;</h3>
@@ -50,7 +53,7 @@ export default function Home() {
 
           <a
             href="https://www.instagram.com/nftindonesia_/"
-            className="{styles.card}"
+            className={styles.card}
             target="_blank"
           >
             <h3>Instagram &rarr;</h3>
@@ -59,7 +62,7 @@ export default function Home() {
 
           <a
             href="https://discord.gg/3AvJSYPJK6"
-            className="{styles.card}"
+            className={styles.card}
             target="_blank"
           >
             <h3>Discord Server &rarr;</h3>
@@ -68,16 +71,17 @@ export default function Home() {
 
           <a
             href="https://warpcast.com/nftindonesia"
-            className="{styles.card}"
+            className={styles.card}
             target="_blank"
           >
             <h3>Farcaster &rarr;</h3>
-            <p>A link to official Farcaster account of NFT Indonesia.</p>
+            <p>
+              A link to official Farcaster account of NFT Indonesia.</p>
           </a>
 
           <a
             href="https://thehug.xyz/collectives/NFTindonesia_"
-            className="{styles.card}"
+            className={styles.card}
             target="_blank"
           >
             <h3>HUG Collective &rarr;</h3>
@@ -86,13 +90,11 @@ export default function Home() {
 
           <a
             href="mailto: zortan@nftindonesia.xyz"
-            className="{styles.card}"
+            className={styles.card}
             target="_blank"
           >
             <h3>Email Address &rarr;</h3>
-            <p>
-              A link to send message to official email address of NFT Indonesia.
-            </p>
+            <p>A link to send message to official email address of NFT Indonesia.</p>
           </a>
         </div>
       </main>
@@ -108,58 +110,54 @@ export default function Home() {
         </a>
       </footer>
 
-      <style jsx>
-        {`
-          main {
-            padding: 5rem 0;
-            flex: 1;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-          }
-          footer {
-            width: 100%;
-            height: 100px;
-            border-top: 1px solid #eaeaea;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-          }
-          footer img {
-            margin-left: 0.5rem;
-            margin-right: 0.5rem;
-          }
-          footer a {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            text-decoration: none;
-            color: inherit;
-          }
-          code {
-            background: #fafafa;
-            border-radius: 5px;
-            padding: 0.75rem;
-            font-size: 1.1rem;
-            font-family: Fira Code, monospace;
-          }
-        `}
-      </style>
+      <style jsx>{`
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        footer img {
+          margin-left: 0.5rem;
+          margin-right: 0.5rem;
+        }
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          text-decoration: none;
+          color: inherit;
+        }
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family: Fira Code, monospace;
+        }
+      `}</style>
 
-      <style jsx global>
-        {`
-          html,
-          body {
-            padding: 0;
-            margin: 0;
-            font-family: Fira Code, monospace;
-          }
-          * {
-            box-sizing: border-box;
-          }
-        `}
-      </style>
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: Fira Code, monospace;
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
     </div>
   );
 }
