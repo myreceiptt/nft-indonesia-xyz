7adab3178cb7f7b1d563f283c5c5d3dfde212b9a
myreceiptt <nota@straight-line.org>
2024-08-31 19:04:53 +0700

    table of content

--- README.md
+++ README.md
@@ -16,6 +16,9 @@ You can start editing the page by modifying `app/page.js`. The page auto-updates
 
 This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
 
+Images storage list:  
+- https://ipfs.io/ipfs/Qme4RHiGt6CoreJd6aeBiZ9K1avsN89uwVWcE1WJzodMJd/
+
 Regards,
 
 Prof. NOTA
--- src/app/blogs/[slug]/page.js
+++ src/app/blogs/[slug]/page.js
@@ -125,7 +125,7 @@ export default function BlogPage({ params }) {
         <div className="grid grid-cols-12 gap-y-8 lg:gap-8 sxl:gap-16 mt-8 px-5 md:px-10">
           <div className="col-span-12  lg:col-span-4">
             <details
-              className="border-[1px] border-solid border-dark dark:border-light text-dark dark:text-light rounded-lg p-4 sticky top-6 max-h-[80vh] overflow-hidden overflow-y-auto"
+              className="border-[1px] border-solid border-dark dark:border-light text-dark dark:text-light rounded-lg p-4 sticky top-28 max-h-[80vh] overflow-hidden overflow-y-auto"
               open
             >
               <summary className="text-lg font-semibold capitalize cursor-pointer">
