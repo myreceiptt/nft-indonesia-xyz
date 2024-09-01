003fbc0a3983dd3888f223381c6e362a2a93716c
myreceiptt <nota@straight-line.org>
2024-09-02 00:17:01 +0700

    end-of-january-2024-collective-update

--- .contentlayer/generated/Blog/_index.mjs
+++ .contentlayer/generated/Blog/_index.mjs
@@ -2,22 +2,23 @@
 
 import adeSantoraPhotoblocGallery__indexMdx from './ade-santora-photobloc-gallery__index.mdx.json' assert { type: 'json' }
 import adiPhotoblocByNftIndonesia__indexMdx from './adi-photobloc-by-nft-indonesia__index.mdx.json' assert { type: 'json' }
-import amaBangkitDanBertahanEvent__indexMdx from './ama-bangkit-dan-bertahan-event__index.mdx.json' assert { type: 'json' }
-import amaWithAjaibKripto__indexMdx from './ama-with-ajaib-kripto__index.mdx.json' assert { type: 'json' }
 import amaWithBananowLand__indexMdx from './ama-with-bananow-land__index.mdx.json' assert { type: 'json' }
+import amaWithAjaibKripto__indexMdx from './ama-with-ajaib-kripto__index.mdx.json' assert { type: 'json' }
 import aprilEndersIsHere__indexMdx from './april-enders-is-here__index.mdx.json' assert { type: 'json' }
+import amaBangkitDanBertahanEvent__indexMdx from './ama-bangkit-dan-bertahan-event__index.mdx.json' assert { type: 'json' }
 import bafomEthByPenahitamArts__indexMdx from './bafom-eth-by-penahitam-arts__index.mdx.json' assert { type: 'json' }
 import bananowLandWhatPlaceIsThat__indexMdx from './bananow-land-what-place-is-that__index.mdx.json' assert { type: 'json' }
 import collectiveUpdateDecember_2023__indexMdx from './collective-update-december-2023__index.mdx.json' assert { type: 'json' }
+import collectiveUpdateEarlyJanuary_2024__indexMdx from './collective-update-early-january-2024__index.mdx.json' assert { type: 'json' }
 import collectiveUpdateVoteVoteVote__indexMdx from './collective-update-vote-vote-vote__index.mdx.json' assert { type: 'json' }
 import dewanPerwakilanDripNyaIndonesia__indexMdx from './dewan-perwakilan-drip-nya-indonesia__index.mdx.json' assert { type: 'json' }
-import diPenghujungMei_2024__indexMdx from './di-penghujung-mei-2024__index.mdx.json' assert { type: 'json' }
 import earlierThisAprilFromOurCollectives__indexMdx from './earlier-this-april-from-our-collectives__index.mdx.json' assert { type: 'json' }
+import diPenghujungMei_2024__indexMdx from './di-penghujung-mei-2024__index.mdx.json' assert { type: 'json' }
 import earlierThisMarchFromOurCollectives__indexMdx from './earlier-this-march-from-our-collectives__index.mdx.json' assert { type: 'json' }
-import endOfMarchIsHere__indexMdx from './end-of-march-is-here__index.mdx.json' assert { type: 'json' }
-import khugaNftAma__indexMdx from './khuga-nft-ama__index.mdx.json' assert { type: 'json' }
 import galleryPhotoblocOfYoghiJuliansa__indexMdx from './gallery-photobloc-of-yoghi-juliansa__index.mdx.json' assert { type: 'json' }
+import khugaNftAma__indexMdx from './khuga-nft-ama__index.mdx.json' assert { type: 'json' }
 import khugaNftLetTheBattleBegins__indexMdx from './khuga-nft-let-the-battle-begins__index.mdx.json' assert { type: 'json' }
+import endOfMarchIsHere__indexMdx from './end-of-march-is-here__index.mdx.json' assert { type: 'json' }
 import lateFebruaryCollectivePageUpdate__indexMdx from './late-february-collective-page-update__index.mdx.json' assert { type: 'json' }
 import nftIndonesiaProgressiveWebApp__indexMdx from './nft-indonesia-progressive-web-app__index.mdx.json' assert { type: 'json' }
 import mindblowonToys__indexMdx from './mindblowon-toys__index.mdx.json' assert { type: 'json' }
@@ -27,11 +28,11 @@ import photoblocAgungNWibowo__indexMdx from './photobloc-agung-n-wibowo__index.m
 import photoblocOfAdeAndryani__indexMdx from './photobloc-of-ade-andryani__index.mdx.json' assert { type: 'json' }
 import photoblocOfGraceAnata__indexMdx from './photobloc-of-grace-anata__index.mdx.json' assert { type: 'json' }
 import photoblocOfJuanGirsang__indexMdx from './photobloc-of-juan-girsang__index.mdx.json' assert { type: 'json' }
+import photoblocOfKurniaNgayugaWibowo__indexMdx from './photobloc-of-kurnia-ngayuga-wibowo__index.mdx.json' assert { type: 'json' }
 import photoblocOfTopatiers__indexMdx from './photobloc-of-topatiers__index.mdx.json' assert { type: 'json' }
 import revolusiHijauAlamHijauAnagata__indexMdx from './revolusi-hijau-alam-hijau-anagata__index.mdx.json' assert { type: 'json' }
-import photoblocOfKurniaNgayugaWibowo__indexMdx from './photobloc-of-kurnia-ngayuga-wibowo__index.mdx.json' assert { type: 'json' }
-import satuSekteXAjaibKriptoXNftIndonesia__indexMdx from './satu-sekte-x-ajaib-kripto-x-nft-indonesia__index.mdx.json' assert { type: 'json' }
 import updateFebruary_2024FromOurCollective__indexMdx from './update-february-2024-from-our-collective__index.mdx.json' assert { type: 'json' }
-import collectiveUpdateEarlyJanuary_2024__indexMdx from './collective-update-early-january-2024__index.mdx.json' assert { type: 'json' }
+import satuSekteXAjaibKriptoXNftIndonesia__indexMdx from './satu-sekte-x-ajaib-kripto-x-nft-indonesia__index.mdx.json' assert { type: 'json' }
+import endOfJanuary_2024CollectiveUpdate__indexMdx from './end-of-january-2024-collective-update__index.mdx.json' assert { type: 'json' }
 
-export const allBlogs = [adeSantoraPhotoblocGallery__indexMdx, adiPhotoblocByNftIndonesia__indexMdx, amaBangkitDanBertahanEvent__indexMdx, amaWithAjaibKripto__indexMdx, amaWithBananowLand__indexMdx, aprilEndersIsHere__indexMdx, bafomEthByPenahitamArts__indexMdx, bananowLandWhatPlaceIsThat__indexMdx, collectiveUpdateDecember_2023__indexMdx, collectiveUpdateVoteVoteVote__indexMdx, dewanPerwakilanDripNyaIndonesia__indexMdx, diPenghujungMei_2024__indexMdx, earlierThisAprilFromOurCollectives__indexMdx, earlierThisMarchFromOurCollectives__indexMdx, endOfMarchIsHere__indexMdx, khugaNftAma__indexMdx, galleryPhotoblocOfYoghiJuliansa__indexMdx, khugaNftLetTheBattleBegins__indexMdx, lateFebruaryCollectivePageUpdate__indexMdx, nftIndonesiaProgressiveWebApp__indexMdx, mindblowonToys__indexMdx, pandawaNftByDeksaProject__indexMdx, penulisanArtikelDiSini__indexMdx, photoblocAgungNWibowo__indexMdx, photoblocOfAdeAndryani__indexMdx, photoblocOfGraceAnata__indexMdx, photoblocOfJuanGirsang__indexMdx, photoblocOfTopatiers__indexMdx, revolusiHijauAlamHijauAnagata__indexMdx, photoblocOfKurniaNgayugaWibowo__indexMdx, satuSekteXAjaibKriptoXNftIndonesia__indexMdx, updateFebruary_2024FromOurCollective__indexMdx, collectiveUpdateEarlyJanuary_2024__indexMdx]
+export const allBlogs = [adeSantoraPhotoblocGallery__indexMdx, adiPhotoblocByNftIndonesia__indexMdx, amaWithBananowLand__indexMdx, amaWithAjaibKripto__indexMdx, aprilEndersIsHere__indexMdx, amaBangkitDanBertahanEvent__indexMdx, bafomEthByPenahitamArts__indexMdx, bananowLandWhatPlaceIsThat__indexMdx, collectiveUpdateDecember_2023__indexMdx, collectiveUpdateEarlyJanuary_2024__indexMdx, collectiveUpdateVoteVoteVote__indexMdx, dewanPerwakilanDripNyaIndonesia__indexMdx, earlierThisAprilFromOurCollectives__indexMdx, diPenghujungMei_2024__indexMdx, earlierThisMarchFromOurCollectives__indexMdx, galleryPhotoblocOfYoghiJuliansa__indexMdx, khugaNftAma__indexMdx, khugaNftLetTheBattleBegins__indexMdx, endOfMarchIsHere__indexMdx, lateFebruaryCollectivePageUpdate__indexMdx, nftIndonesiaProgressiveWebApp__indexMdx, mindblowonToys__indexMdx, pandawaNftByDeksaProject__indexMdx, penulisanArtikelDiSini__indexMdx, photoblocAgungNWibowo__indexMdx, photoblocOfAdeAndryani__indexMdx, photoblocOfGraceAnata__indexMdx, photoblocOfJuanGirsang__indexMdx, photoblocOfKurniaNgayugaWibowo__indexMdx, photoblocOfTopatiers__indexMdx, revolusiHijauAlamHijauAnagata__indexMdx, updateFebruary_2024FromOurCollective__indexMdx, satuSekteXAjaibKriptoXNftIndonesia__indexMdx, endOfJanuary_2024CollectiveUpdate__indexMdx]
