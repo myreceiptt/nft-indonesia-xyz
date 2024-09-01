d5dc60105e2d0b0433a45954356ad4bb09eeafb1
myreceiptt <nota@straight-line.org>
2024-09-01 22:46:01 +0700

    width="747"

--- src/components/Blog/VideoSource.js
+++ src/components/Blog/VideoSource.js
@@ -1,7 +1,7 @@
 export default function VideoSource({ xxx }) {
   return (
     <div>
-      <video className="aspect-video w-full" controls preload="auto" loop>
+      <video className="aspect-video" width="747" controls preload="auto" loop>
         <source src={xxx} type="video/mp4" />
       </video>
     </div>
