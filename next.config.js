db13742e66bccfae6c3b3b0f5c8ee5949cf6be4d
myreceiptt <nota@endhonesa.com>
2025-12-21 23:58:15 +0700

    Include content files in Next build tracing

--- next.config.js
+++ next.config.js
@@ -4,6 +4,12 @@ const nextConfig = {
   compiler: {
     removeConsole: true,
   },
+  outputFileTracingIncludes: {
+    "/": ["content/**"],
+    "/blogs/[slug]": ["content/**"],
+    "/(insightfull)/categories/[slug]": ["content/**"],
+    "/(insightfull)/about-us": ["content/**"],
+  },
   images: {
     remotePatterns: [
       {
